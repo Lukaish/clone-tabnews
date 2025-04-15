9877942f3b748a176f561a17efc0350e9af53f18
Lukaish <lukassilvabernardi@outlook.com>
2025-04-15 20:04:43 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to api/v1/status should retun 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const resBody = await res.json();
+  expect(resBody.updated_at).toBeDefined();
+
+  const parseUpdateAt = new Date(resBody.updated_at).toISOString();
+  expect(resBody.updated_at).toEqual(parseUpdateAt);
+
+  expect(resBody.dependecies.database.version).toEqual("17.4");
+  expect(resBody.dependecies.database.max_connections).toEqual(100);
+  expect(resBody.dependecies.database.opened_connections).toEqual(1);
+  // expect(Object.keys(resBody.dependecies.database).length).toEqual(3);
 });
