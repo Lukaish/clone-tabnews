493f82e4abd16c7be57a923b7d87733c2e6a64cd
Lukaish <lukassilvabernardi@outlook.com>
2025-05-06 22:10:35 -0300

    Add tests get/post and modificate `package.json`

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,10 @@
+test("POST to api/v1/migrations should retun 200", async () => {
+  const res = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(res.status).toBe(200);
+
+  const resBody = await res.json();
+  console.log(resBody);
+  expect(Array.isArray(resBody)).toBe(true);
+});
