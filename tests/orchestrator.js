b6039c246efe4f4e4931af3e16bc39eb5b342605
Lukasih <lukassilvabernardi@outlook.com>
2025-08-24 15:48:59 -0300

    commit to fully merged

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -3,7 +3,6 @@ import retry from "async-retry";
 async function waitForAllServices() {
   await waitForWebServer();
 
-  //Fica tentado consultar o endpoint do /status, e só quando conseguir com sucesso, irá desprender o script
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
@@ -12,7 +11,6 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      // const responseBody = await response.json();
 
       if (response.status !== 200) {
         throw Error();
